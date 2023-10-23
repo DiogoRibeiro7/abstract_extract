fbfdb9485fe620069afdd39f7dc4438e63b255b8
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-10-23 12:59:27 +0100

    chore: author names

--- abstract_refined.py
+++ abstract_refined.py
@@ -1,55 +1,102 @@
 import requests
 
 from requests.exceptions import HTTPError
+from typing import Optional, Dict
 
-def fetch_from_scopus(query, api_key, max_results=25):
+
+import requests
+from typing import Optional, Dict, List
+
+def fetch_all_from_scopus(query: str, api_key: str, start_date: Optional[str] = None,
+                          end_date: Optional[str] = None, author: Optional[str] = None) -> Optional[List[Dict]]:
     """
-    Fetches articles from Scopus based on a query.
+    Fetches all articles from Scopus based on a query and additional conditions.
     
     Args:
     - query (str): Search query for Scopus.
     - api_key (str): Your Scopus API key.
-    - max_results (int): Maximum number of articles to fetch.
+    - start_date (str, optional): Start date for filtering articles. Format YYYY-MM-DD. Defaults to None.
+    - end_date (str, optional): End date for filtering articles. Format YYYY-MM-DD. Defaults to None.
+    - author (str, optional): Author name to filter articles. Defaults to None.
     
     Returns:
-    - dict: Dictionary containing fetched articles and metadata.
+    - List[Dict]: List of dictionaries containing fetched articles and metadata, or None in case of an error.
     """
+    # Base URL for the Scopus API
     base_url = "https://api.elsevier.com/content/search/scopus"
     
+    # Headers for the API request
     headers = {
         "Accept": "application/json",
         "X-ELS-APIKey": api_key
     }
     
+    # Basic parameters for the API request
     params = {
         "query": query,
-        "count": max_results
+        "count": 25,  # Maximum allowed per request
+        "cursor": "*"  # Initialize cursor
     }
     
+    # Add date range to the query if specified
+    if start_date and end_date:
+        params["date"] = f"{start_date} to {end_date}"
+        
+    # Add author to the query if specified
+    if author:
+        params["query"] += f" AND AUTHOR({author})"
+        
+    all_results = []  # To store all fetched articles
+    
     try:
-        response = requests.get(base_url, headers=headers, params=params)
-        response.raise_for_status()  # Ensure the request was successful
-        return response.json()
+        while True:
+            # Make the API request
+            response = requests.get(base_url, headers=headers, params=params)
+            response.raise_for_status()  # Check if the request was successful
+            
+            json_response = response.json()
+            all_results.extend(json_response.get("search-results", {}).get("entry", []))
+            
+            next_cursor = json_response.get("search-results", {}).get("cursor", {}).get("@next")
+            if next_cursor:
+                params["cursor"] = next_cursor  # Update cursor for the next iteration
+            else:
+                break  # Exit loop if there's no more data to fetch
+                
+        return all_results
     except requests.RequestException as e:
         print(f"Error fetching from Scopus: {e}")
         if response.status_code != 200:
             print(f"Response Code: {response.status_code}")
-            print(response.text)  # This will print the error message from Scopus
+            print(response.text)  # Print the error message from Scopus
         return None
+
+
 
 # Example usage
 api_key = "aa945f2068e32283437cea4dc191245b"
-query = "aldina correia"
-articles = fetch_from_scopus(query, api_key)
+# The query you want to perform
+query = "Aldina Correia"
+
+# Optionally, specify a date range and author
+# start_date = "2010-01-01"
+# end_date = "2023-01-01"
+# author = "Aldina Correia"
+
+# Call the function
+result = fetch_all_from_scopus(query, api_key, author=author)
+
+
+print(result)
 
 
 def process_scopus_response(articles):
     """
     Processes the Scopus API response to extract key article details.
-    
+
     Args:
     - articles (list): List of dictionaries containing articles from Scopus.
-    
+
     Returns:
     - list: List of dictionaries containing processed article details.
     """
@@ -60,13 +107,12 @@ def process_scopus_response(articles):
         title = article.get("dc:title", None)
         abstract = article.get("dc:description", None)
         publication_date = article.get("prism:coverDate", None)
-        
+
         # Extracting author details
         authors = article.get("author", [])
-        print(authors)
         author_names = [author.get("authname", "") for author in authors]
         doi = article.get("prism:doi", None)
-        
+
         # Append the processed article details to the list
         processed_articles.append({
             "title": title,
@@ -78,6 +124,7 @@ def process_scopus_response(articles):
 
     return processed_articles
 
+
 # Process the fetched articles
 processed_data = process_scopus_response(articles)
 
@@ -85,11 +132,11 @@ processed_data = process_scopus_response(articles)
 def get_abstract_from_doi(doi):
     # Define the CrossRef API endpoint
     url = f"https://api.crossref.org/works/{doi}"
-    
+
     try:
         # Make the API request
         response = requests.get(url)
-        
+
         # Raise an HTTPError if the HTTP request returned an unsuccessful status code
         response.raise_for_status()
 
@@ -102,14 +149,14 @@ def get_abstract_from_doi(doi):
         # Handle other exceptions
         print(f"An error occurred: {err}")
         return "Error"
-        
+
     data = response.json()
-    
+
     # Extract the abstract, if available
     abstract = data['message'].get('abstract', 'Abstract not available.')
-    print(data)
-    
+
     return abstract
+
 
 # Example
 for article in processed_data:
@@ -129,4 +176,3 @@ for article in processed_data:
     print(f"Publication Date: {first_article['publication_date']}")
     print(f"Authors: {', '.join(first_article['authors'])}")
     print(f"Doi: {first_article['doi']}")
-
